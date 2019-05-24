79d75d94594bc0b324bf396dfe8c763b7a5cd846
Karl Beksa <karlbeksa@gmail.com>
2019-05-24 10:09:05 -0400

    Final Product

--- component/Tabs.js
+++ component/Tabs.js
@@ -1,49 +1,46 @@
 class TabLink {
-    constructor(tabElement){
+   constructor(tabElement){
       
       this.tabElement = tabElement;
-     
+
       this.data = this.tabElement.dataset.tab;
 
-      if(this.tabElement.dataset.tab === "all"){
-         this.cards = document.querySelectorAll('.card');
-      } else {
-         this.cards = document.querySelectorAll(`.card[data-tab="${this.data}"`);
-      }
-      
-       this.cards = Array.from(this.cards).map(card => new TabCard(card));
-  
-       this.tabElement.addEventListener('click', () => this.selectTab());
-    }
-  
-    selectTab(){
-  
-       const tabs = document.querySelectorAll('.tab');
-      
-       tabs.forEach(tab => tab.classList.remove('active-tab'));
-  
-       const cards = document.querySelectorAll('.card');
-  
-       cards.forEach(card => card.style.display = 'none');
-      
-       this.tabElement.classList.add('active-tab');
-  
-    
-       this.cards.forEach(card => card.selectCard());
-    }
-  }
-  
-  class TabCard {
-    constructor(cardElement){
-       this.cardElement = cardElement;
-    }
-    selectCard(){
-       this.cardElement.style.display = 'flex';
-    }
-  
-  }
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.data}"`);
 
-  let tabs = document.querySelectorAll('.tab');
+      
+      this.cards = Array.from(this.cards).map(card => new TabCard(card));
+
+      this.tabElement.addEventListener('click', () => this.selectTab());
+   }
+
+   selectTab(){
+
+      const tabs = document.querySelectorAll('.tab');
+      
+      tabs.forEach(tab => tab.classList.remove('active-tab'));
+
+      const cards = document.querySelectorAll('.card');
+
+      cards.forEach(card => card.style.display = 'none');
+      
+      this.tabElement.classList.add('active-tab');
+
+
+      this.cards.forEach(card => card.selectCard());
+   }
+}
+
+class TabCard {
+   constructor(cardElement){
+      this.cardElement = cardElement;
+   }
+   selectCard(){
+      this.cardElement.style.display = 'flex';
+   }
+  
+}
+
+let tabs = document.querySelectorAll('.tab');
 tabs.forEach(function(tab){
-  return new TabLink(tab);
+   return new TabLink(tab);
 })
